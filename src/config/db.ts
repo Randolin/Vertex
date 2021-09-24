bcdde2f074470288ea200954eefdab1595b60521
randolin <aaron.g.holloway@gmail.com>
2021-09-24 15:36:07 -0700

    Fix server start script and local db ref.

--- src/config/db.ts
+++ src/config/db.ts
@@ -2,8 +2,8 @@ import mongoose from 'mongoose';
 
 const connectDB = async (): Promise<void> => {
   try {
-    // database Name
-    const con = await mongoose.connect(process.env.MONGODB_URI || '');
+    const databaseName = 'moxy';
+    const con = await mongoose.connect(process.env.MONGODB_URI || `mongodb://127.0.0.1:27017/${databaseName}`);
     console.log(`Database connected : ${con.connection.host}`);
   } catch (error: any) {
     console.error(`Error: ${error.message}`);
