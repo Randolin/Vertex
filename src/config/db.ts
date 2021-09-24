2ae5238a0d35935a15aba81273e64a81c6c262d4
randolin <aaron.g.holloway@gmail.com>
2021-09-24 15:25:59 -0700

    Changed DB target to Atlas.

--- src/config/db.ts
+++ src/config/db.ts
@@ -1,10 +1,9 @@
 import mongoose from 'mongoose';
 
-const connectDB = async () => {
+const connectDB = async (): Promise<void> => {
   try {
     // database Name
-    const databaseName = 'moxy';
-    const con = await mongoose.connect(`mongodb://127.0.0.1:27017/${databaseName}`);
+    const con = await mongoose.connect(process.env.MONGODB_URI || '');
     console.log(`Database connected : ${con.connection.host}`);
   } catch (error: any) {
     console.error(`Error: ${error.message}`);
--- src/server.ts
+++ src/server.ts
@@ -13,12 +13,13 @@ const app = express();
 app.use('/api', router);
 app.use(express.static(path.join(__dirname, 'client', 'build')));
 
-const PORT = process.env.PORT || 5000;
+const secret = process.env.SECRET || 'ugly space wizard donkey';
+const port = process.env.PORT || 5000;
 
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'));
 });
 
-app.listen(PORT, () => {
-  console.log(`App is running in ${process.env.NODE_ENV} mode on port ${PORT}`);
+app.listen(port, () => {
+  console.log(`App is running in ${process.env.NODE_ENV} mode on port ${port}`);
 });
