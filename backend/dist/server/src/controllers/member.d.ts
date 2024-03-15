88e9f69b61c606740974a52e3d639d9a327abe39
randolin <aaron.g.holloway@gmail.com>
2024-03-14 22:24:59 -0700

    Removed accidental dist folder.

--- backend/dist/server/src/controllers/member.d.ts
+++
@@ -1,2 +0,0 @@
-export declare const getMembers: import("express").RequestHandler<import("express-serve-static-core").ParamsDictionary, any, any, import("qs").ParsedQs, Record<string, any>>;
-export declare const getMemberById: import("express").RequestHandler<import("express-serve-static-core").ParamsDictionary, any, any, import("qs").ParsedQs, Record<string, any>>;
