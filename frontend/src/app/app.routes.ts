fd755759fd1befd0561e8167257d34727c9de558
randolin <aaron.g.holloway@gmail.com>
2024-03-15 17:35:35 -0700

    Getting frontend blocked out.

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
+import { LandingPageComponent } from './pages/landing-page/landing-page.component';
+import { ChartPageComponent } from './pages/chart-page/chart-page.component';
+import { AboutPageComponent } from './pages/about-page/about-page.component';
+import { NotFoundPageComponent } from './pages/not-found-page/not-found-page.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: '', component: LandingPageComponent},
+    {path: 'about', component: AboutPageComponent},
+    {path: 'chart', component: ChartPageComponent},
+    {path: '**', component: NotFoundPageComponent},
+];
